4671afedf114a0a925da709c976a862e9643b7e7
noClaps <github@zerolimits.dev>
2023-02-10 23:38:35 +0000

    Remove subdir as base url
    The site is intended to be the homepage, so it doesn't make much sense for it to be served under a subdirectory

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,5 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://noClaps.github.io",
-  base: "/links-template-astro",
   integrations: [sitemap(), critters(), compress()]
 });
